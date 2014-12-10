b846cdf1f104c7b6fbf088684336f09d455c130a
Antonio Nacarino de los Santos <nakasoft@gmail.com>
2014-12-10 01:01:55 +0100

    Home controller

--- cruddb/public/index.php
+++ cruddb/public/index.php
@@ -9,11 +9,15 @@ $config = moduleManager(__DIR__.'/../configs/global.php');
 
 Session_start();
 
-
-
 switch($request['controller'])
 {
     default:
+    case 'home':
+        ob_start();
+        include_once '../modules/Application/src/Application/controllers/home.php';
+        $view=ob_get_contents();
+        ob_end_clean();
+    break;
 //     case 'authentication':
 //             ob_start();
 //             include_once '../modules/Application/src/Application/controllers/authentication.php';
@@ -25,15 +29,17 @@ switch($request['controller'])
             include_once '../modules/Application/src/Application/controllers/users.php';
         $view=ob_get_contents();
         ob_end_clean();
-    break;
+        //include_once '../modules/Application/src/Application/layouts/dashboard.phtml';
+        break;
     case 'error':
         ob_start();
             include_once '../modules/Application/src/Application/controllers/error.php';
         $view=ob_get_contents();
         ob_end_clean();
-    break;
+        //include_once '../modules/Application/src/Application/layouts/dashboard.phtml';
+        break;
 }
 
 
-include_once '../modules/Application/src/Application/layouts/dashboard.phtml';
-
+//include_once '../modules/Application/src/Application/layouts/dashboard.phtml';
+include_once '../modules/Application/src/Application/layouts/home.phtml';
