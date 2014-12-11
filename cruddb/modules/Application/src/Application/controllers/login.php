652abff1090d747f39d7111cf534b7cdbbed1a4e
Pablo <pablito_azn@hotmail.com>
2014-12-11 09:18:30 +0100

    little fix

--- cruddb/modules/Application/src/Application/controllers/login.php
+++ cruddb/modules/Application/src/Application/controllers/login.php
@@ -3,6 +3,6 @@
 switch ($request['action']){
     
    case 'index':
-       include('../modules/Application/src/Application/views/users/login.phtml');
+       include('../modules/Application/src/Application/views/login/index.phtml');
    break;
 }
